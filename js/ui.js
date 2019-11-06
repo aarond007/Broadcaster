3e316b80caa0f4d654b147a534687ad835afeb3c
aarond007 <lastonline99@gmail.com>
2019-11-06 09:32:48 +0200

    add read redflag/intervenee

--- js/ui.js
+++ js/ui.js
@@ -18,23 +18,49 @@ const task=document.querySelector('.tasks');
 div.appendChild(document.querySelector('.task-title'));
 div.appendChild(task);
 div.appendChild(document.querySelector('.footer'));
+//switch displays
+const contentdisplays=(par)=>{
+	if(par=="btn"){
+		document.querySelector(".redflagcontainer").style.display="none";
+		document.querySelector(".redflagcontent").style.display="block";
+		document.querySelector(".viewredflag").style.display="none";
+	}
+	else if(par=="li"){
+		document.querySelector(".redflagcontainer").style.display="block";
+		document.querySelector(".redflagcontent").style.display="none";
+		document.querySelector(".viewredflag").style.display="none";
+	}else{
+		document.querySelector(".redflagcontainer").style.display="none";
+		document.querySelector(".redflagcontent").style.display="none";
+		document.querySelector(".viewredflag").style.display="block";
+	}
+}
 // default
-let deft="redflag";
+let deft="red-flag";
 // red flag or intervention
 const flag=document.querySelectorAll('.flag-title');
 const redflag=document.querySelector('.flag-title.red');
 const int=document.querySelector('.flag-title.int');
 redflag.classList.add('active');
 const content=(contentvalue)=>{
-	var listitem=["resolved", "under investigation", "rejected"];
-	document.querySelector('.btnnew').textContent=`New ${contentvalue}`
+	const listitem=["resolved", "under investigation", "rejected", "list"];
+	const btnnew=document.querySelector('.btnnew');
+	btnnew.textContent=`New ${contentvalue}`
+	btnnew.addEventListener('click', (e)=>{
+		contentdisplays('btn');
+	})
 	const li=document.querySelectorAll('.redflags');
 	li.forEach((list, index)=>{
 		list.textContent=`${contentvalue} ${listitem[index]}`;
+		list.addEventListener('click', (e)=>{
+			contentdisplays('li');
+		})
 	})
 	document.querySelector('.redflagbtn').textContent=`Create ${contentvalue}`	;
 };
+
 content(deft);
+// switch menus
 flag.forEach((menu)=>{
 	menu.addEventListener('click',(e)=>{
 		if(e.target==redflag){
@@ -42,16 +68,23 @@ flag.forEach((menu)=>{
 			redflag.classList.add('active');
 			int.classList.remove('active');
 			content(deft);
+			contentdisplays('btn');
 		}
 		else{
 			deft="intervention";
 			redflag.classList.remove('active');
 			int.classList.add('active');
 			content(deft);
+			contentdisplays('btn');
 		}	
 	})
-
-
-
 })
-
+const redlist=document.querySelector('.redflaglist');
+const redtitle=document.querySelectorAll('.redtitle');
+const reddesc=document.querySelectorAll('.reddesc');
+const redflagcontainer=document.querySelector('.redflagcontainer');
+reddesc.forEach((desc)=>{
+	desc.addEventListener('click',(e)=>{
+		contentdisplays('message');
+	})
+})
